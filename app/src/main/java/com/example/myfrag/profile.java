16c8f33d25ac41e4fe9289e64861280acd1984de
kushagra <kushgupta66@gmail.com>
2020-02-11 23:54:24 +0530

    issue with followers list recyclerview parsed data

--- app/src/main/java/com/example/myfrag/profile.java
+++ app/src/main/java/com/example/myfrag/profile.java
@@ -12,6 +12,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -20,10 +22,15 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.myfrag.Adapter.RecyclerViewAdapter;
+import com.example.myfrag.Adapter.example_item;
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import static android.content.ContentValues.TAG;
 
@@ -34,8 +41,15 @@ public class profile extends Fragment {
     TextView editText1;
     TextView textView;
     TextView textView1;
+    TextView textView2;
+    TextView textView3;
+    private RecyclerView mrecyclerview;
+    private RecyclerViewAdapter mrecycleadapter;
+    private RecyclerView.LayoutManager layoutManager;
+
 
     private String url= "https://api.github.com/users/akashsarkar188";
+    private String url1= "https://api.github.com/users/akashsarkar188/followers";
     private RequestQueue mRequestQueue;
     private StringRequest mStringRequest;
 
@@ -47,6 +61,7 @@ public class profile extends Fragment {
 
 
         sendAndRequestResponse();
+        sendAndRequestResponse1();
 
 
         // Inflate the layout for this fragment
@@ -82,18 +97,26 @@ public class profile extends Fragment {
                             String githublink=response.getString("blog");
                             String loc=response.getString("location");
                             String bio=response.getString("bio");
-
+                            String follow=response.getString("followers");
+                            String following=response.getString("following");
 
                             editText=root.findViewById(R.id.editText);
                             editText1=root.findViewById(R.id.editText2);
                             textView=root.findViewById(R.id.textView);
                             textView1=root.findViewById(R.id.textView3);
+                            textView2=root.findViewById(R.id.textView6);
+                            textView3=root.findViewById(R.id.textView7);
                             ImageView imageView = root.findViewById(R.id.imageView2);
                             editText.setText(name);
                             textView.setText(loc);
                             textView1.setText(bio);
+                            textView2.setText("Followers : "+follow);
+                            textView3.setText("Following : "+following);
                             editText1.setText(githublink);
                             Picasso.get().load(url).into(imageView);
+
+
+
 
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -111,6 +134,80 @@ public class profile extends Fragment {
 
     }
 
+    private void sendAndRequestResponse1() {
+
+        //RequestQueue initialized
+        mRequestQueue = Volley.newRequestQueue(getActivity());
+
+//RequestQueue initialized
+        mRequestQueue = Volley.newRequestQueue(getActivity());
+
+        //String Request initialized
+        mStringRequest = new StringRequest(Request.Method.GET, url1, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                            ArrayList<example_item> exampleList = new ArrayList<>();
+                        /*    ImageView imageView = root.findViewById(R.id.imageView2);
+
+                            Picasso.get().load(picx).into(imageView);
+*/                          String str=response;
+                JSONArray jsonArray = null;
+                try {
+                    jsonArray = new JSONArray(str);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject obj = null;
+                    try {
+                        obj = jsonArray.getJSONObject(i);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    try {
+                        String loginname = obj.getString("login");
+
+                        String loginpic = obj.getString("avatar_url");
+                        exampleList.add(new example_item(loginname,loginpic));
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+
+
+
+                              //  String loginname = employee.getString("login");
+                              //  String loginpic = employee.getString("avatar_url");
+                               // exampleList.add(new example_item(loginname,loginpic));
+
+
+                            }
+
+                            mrecyclerview = root.findViewById(R.id.recycler_view1);
+                            mrecyclerview.setHasFixedSize(true);
+                            layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+                            mrecycleadapter = new RecyclerViewAdapter(exampleList);
+                            mrecyclerview.setLayoutManager(layoutManager);
+                            mrecyclerview.setAdapter(mrecycleadapter);
+
+
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                });
+        mRequestQueue.add(mStringRequest);
+
+
+    }
 
 
 }
